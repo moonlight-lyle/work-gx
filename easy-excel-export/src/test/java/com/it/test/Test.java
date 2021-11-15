ed325e8793b232a693c390626d0fd77d5913d707
moonlight-lyle <15755589769@163.com>
2021-11-15 22:34:29 +0800

    字符串集合排序

---
+++ easy-excel-export/src/test/java/com/it/test/Test.java
@@ -0,0 +1,21 @@
+package com.it.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("20211115");
+        list.add("20220115");
+        list.add("20211215");
+        list.add("20230311");
+        list.add("20240215");
+        list.add("20120323");
+        Collections.sort(list);
+        for (String str : list) {
+            System.out.println(str);
+        }
+    }
+}
