28537ecc5b05b94fc5dbaca995c01c7afc4eca1d
moonlight-lyle <15755589769@163.com>
2022-04-02 20:49:27 +0800

    Stream

--- javaSE/src/main/java/com/it/stream/StreamDemo.java
+++ javaSE/src/main/java/com/it/stream/StreamDemo.java
@@ -2,6 +2,7 @@ package com.it.stream;
 
 
 import com.it.pojo.User;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,7 +34,7 @@ public class StreamDemo {
         // 集合收集器
         List<User> userList=new ArrayList<>();
         User user1=new User(18,"张三","湖北");
-        User user2=new User(20,"张铁","湖北");
+        User user2=new User(20,"张铁",null);
         User user3=new User(21,"李四","湖南");
         User user4=new User(25,"李静","广西");
         userList.add(user1);
@@ -41,9 +42,20 @@ public class StreamDemo {
         userList.add(user3);
         userList.add(user4);
         // 收集年龄大于20的用户
-        List<User> users = userList.stream().filter(item -> item.getAge() > 20).collect(Collectors.toList());
-        for (User user : users) {
+//        List<User> users = userList.stream().filter(item -> item.getAge() > 20).collect(Collectors.toList());
+//        for (User user : users) {
+//            System.out.println(user);
+//        }
+
+//        List<String> stringList = userList.stream().map(item -> item.getAge() + item.getName()).collect(Collectors.toList());
+//        for (String s : stringList) {
+//            System.out.println(s);
+//        }
+
+        List<User> collect = userList.stream().filter(item -> StringUtils.isNotEmpty(item.getAddress())).collect(Collectors.toList());
+        for (User user : collect) {
             System.out.println(user);
         }
+
     }
 }
